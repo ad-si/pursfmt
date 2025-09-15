6bc08b2bf215021c5fff4dc3e7e784867e6cdedf
flip111 <flip101@gmail.com>
2025-09-15 10:52:58 +0200

    Add letClauseSameLine

--- bin/src/Bin/FormatOptions.purs
+++ bin/src/Bin/FormatOptions.purs
@@ -25,6 +25,7 @@ type FormatOptions =
   , typeArrowPlacement :: TypeArrowOption
   , unicode :: UnicodeOption
   , width :: Maybe Int
+  , letClauseSameLine :: Boolean
   }
 
 defaults :: FormatOptions
@@ -38,6 +39,7 @@ defaults =
   , typeArrowPlacement: TypeArrowFirst
   , unicode: UnicodeSource
   , width: Nothing
+  , letClauseSameLine: false
   }
 
 formatOptions :: ArgParser FormatOptions
@@ -104,6 +106,10 @@ formatOptions =
           "The maximum width of the document in columns.\nDefaults to no maximum."
           # Arg.int
           # Arg.optional
+    , letClauseSameLine:
+        Arg.flag [ "--let-clause-same-line", "-lcsl" ]
+          "Put source code directly after \"let\" and \"in\" instead of the next line."
+          # Arg.boolean
     }
 
 unicodeOption :: ArgParser UnicodeOption
@@ -133,6 +139,7 @@ fromJson json = do
   typeArrowPlacement <- traverse typeArrowPlacementFromString =<< obj .:? "typeArrowPlacement"
   unicode <- traverse unicodeFromString =<< obj .:? "unicode"
   width <- obj .:? "width"
+  letClauseSameLine <- obj .:? "letClauseSameLine"
   pure
     { importSort: fromMaybe defaults.importSort importSort
     , importWrap: fromMaybe defaults.importWrap importWrap
@@ -143,6 +150,7 @@ fromJson json = do
     , typeArrowPlacement: fromMaybe defaults.typeArrowPlacement typeArrowPlacement
     , unicode: fromMaybe defaults.unicode unicode
     , width: width <|> defaults.width
+    , letClauseSameLine: fromMaybe defaults.letClauseSameLine letClauseSameLine
     }
 
 toJson :: FormatOptions -> Json
@@ -157,6 +165,7 @@ toJson options =
     # extend (assoc "typeArrowPlacement" (typeArrowPlacementToString options.typeArrowPlacement))
     # extend (assoc "unicode" (unicodeToString options.unicode))
     # extend (assoc "width" (maybe jsonNull encodeJson options.width))
+    # extend (assoc "letClauseSameLine" options.letClauseSameLine)
 
 thenPlacementFromString :: String -> Either JsonDecodeError ThenPlacementOption
 thenPlacementFromString = case _ of
